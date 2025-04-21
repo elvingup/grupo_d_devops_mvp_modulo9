d7dba5fa84e2f8f8738848bd93a9e20d75a0e40e
Elvyngthon <el.cayro@gmail.com>
2025-04-21 19:22:11 -0300

    Rename aws_key_pair resource to aws_key_pair_grupo_d for consistency

--- terraform/project/modules/backend/main.tf
+++ terraform/project/modules/backend/main.tf
@@ -75,7 +75,7 @@ resource "tls_private_key" "lb_ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
-resource "aws_key_pair" "lb_ssh_key_pair" {
+resource "aws_key_pair_grupo_d" "lb_ssh_key_pair" {
   key_name   = "key_pair"
   public_key = tls_private_key.lb_ssh_key.public_key_openssh
 }
