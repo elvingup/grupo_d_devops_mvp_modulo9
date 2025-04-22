8683fb121a608770504a6f03d771aa9f6ebb6c6e
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-21 21:33:34 -0300

    Update main.tf
    FIX: correção de erros de nomenclatura

--- terraform/project/modules/database/main.tf
+++ terraform/project/modules/database/main.tf
@@ -16,20 +16,20 @@ resource "aws_security_group" "database_sg" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress_sg_rule" {
-  security_group_id = aws_security_group.backend_sg.id
+  security_group_id = aws_security_group.database_sg.id
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
 } 
 
 resource "aws_vpc_security_group_ingress_rule" "ingress_80_sg_rule" {
-  security_group_id = aws_security_group.backend_sg.id
+  security_group_id = aws_security_group.database_sg.id
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "tcp"
   from_port   = 5000
   to_port     = 5000
 }
 resource "aws_vpc_security_group_ingress_rule" "ingress_22_sg_rule" {
-  security_group_id = aws_security_group.backend_sg.id
+  security_group_id = aws_security_group.database_sg.id
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "tcp"
   from_port   = 22
@@ -40,7 +40,7 @@ resource "aws_instance" "backend_ec2" {
   instance_type = "t3.micro"
   ami = data.aws_ami.imagem_ec2.id
   subnet_id = var.sn_priv01
-  vpc_security_group_ids = [ aws_security_group.backend_sg.id ]
+  vpc_security_group_ids = [ aws_security_group.database_sg.id ]
   key_name = aws_key_pair.lb_ssh_key_pair_grupo_d.key_name
   associate_public_ip_address = true
   tags = {
