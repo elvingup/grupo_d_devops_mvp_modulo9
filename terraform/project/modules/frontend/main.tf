81b07a3357cf615053dc39517fd02b8c66b23f71
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-22 18:55:27 -0300

    Update main.tf

--- terraform/project/modules/frontend/main.tf
+++ terraform/project/modules/frontend/main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "grupo_d_nginx_ec2" {
   ami = data.aws_ami.imagem_ec2.id
   subnet_id = var.sn_pub01
   vpc_security_group_ids = [ aws_security_group.grupo_d_nginx_sg.id ]
-  key_name = data.aws_key_pair.grupo_d_key_pair.key_name
+  key_name =  data.aws_key_pair.lb_ssh_key_pair_grupo_d.key_name
   associate_public_ip_address = true
   tags = {
     Name = "grupo_d-frontend_ec2"
@@ -69,7 +69,7 @@ resource "aws_instance" "grupo_d_nginx_ec2" {
 #  algorithm = "RSA"
 #  rsa_bits  = 2048
 #}
-data "aws_key_pair" "lb_ssh_key_pair_frontend" {
+data "aws_key_pair" "lb_ssh_key_pair_grupo_d" {
   key_name   = "grupo_d_key_pair"
  # public_key = tls_private_key.lb_ssh_key_frontend.public_key_openssh
 }
