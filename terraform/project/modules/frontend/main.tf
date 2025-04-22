9f6676c205ffff7325c9083eb3e543d211a2efe7
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-22 18:28:27 -0300

    Update main.tf

--- terraform/project/modules/frontend/main.tf
+++ terraform/project/modules/frontend/main.tf
@@ -69,7 +69,7 @@ resource "tls_private_key" "lb_ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
-resource "aws_key_pair" "lb_ssh_key_pair_frontend" {
+data "aws_key_pair" "lb_ssh_key_pair_frontend" {
   key_name   = "grupo_d_key_pair"
   public_key = tls_private_key.lb_ssh_key_frontend.public_key_openssh
 }
