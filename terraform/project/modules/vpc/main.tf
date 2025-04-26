693d62b7d59e439bf0f0ab23dc73ef6af8f791a2
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-26 11:40:23 -0300

    Update main.tf

--- terraform/project/modules/vpc/main.tf
+++ terraform/project/modules/vpc/main.tf
@@ -5,36 +5,12 @@ resource "aws_vpc" "grupo_d_vpc" {
   }
 }
 
-resource "aws_subnet" "sn_priv01" {
-  vpc_id = aws_vpc.grupo_d_vpc.id
-  cidr_block = "172.102.1.0/24"
-  availability_zone = "us-west-1b"
-  tags = {
-    Name = "grupo_d-sn_priv01"
-  }
-}
-resource "aws_subnet" "sn_priv02" {
-  vpc_id = aws_vpc.grupo_d_vpc.id
-  cidr_block = "172.102.2.0/24"
-  availability_zone = "us-west-1c"
-  tags = {
-    Name = "grupo_d-sn_priv02"
-  }
-}
-resource "aws_subnet" "sn_pub01" {
-  vpc_id = aws_vpc.grupo_d_vpc.id
+resource "aws_subnet" "sn_pub01" { 
+vpc_id = aws_vpc.grupo_d_vpc.id
   cidr_block = "172.102.3.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "grupo_d-sn_pub01"
-  }
-}
-resource "aws_subnet" "sn_pub02" {
-  vpc_id = aws_vpc.grupo_d_vpc.id
-  cidr_block = "172.102.4.0/24"
-  availability_zone = "us-west-1c"
-  tags = {
-    Name = "grupo_d-sn_pub02"
   }
 }
 
@@ -61,16 +37,4 @@ resource "aws_route_table_association" "pub01assoc" {
   subnet_id = aws_subnet.sn_pub01.id
   route_table_id = aws_route_table.route_pub.id
 }
-resource "aws_route_table_association" "pub02assoc" {
-  subnet_id = aws_subnet.sn_pub02.id
-  route_table_id = aws_route_table.route_pub.id
-}
 
-resource "aws_route_table_association" "priv01assoc" {
-  subnet_id = aws_subnet.sn_priv01.id
-  route_table_id = aws_route_table.route_pub.id
-}
-resource "aws_route_table_association" "priv02assoc" {
-  subnet_id = aws_subnet.sn_priv02.id
-  route_table_id = aws_route_table.route_pub.id
-}
